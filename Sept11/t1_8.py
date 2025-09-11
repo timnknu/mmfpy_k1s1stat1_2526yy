44801bb99eeeb803da2ff9cd966ec442edaa9a8f
Your Name <you@example.com>
2025-09-11 09:12:46 +0300

    upd

---
+++ Sept11/t1_8.py
@@ -0,0 +1,12 @@
+myvar = 782
+# print(myvar // 10)
+dight = myvar // 10
+print(myvar % 10)
+print(dight % 10)
+print(dight // 10)
+
+print('------')
+print(myvar // 100)
+ost = myvar % 100
+print(ost // 10)
+print(ost % 10)
