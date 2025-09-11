2d0d57f30cf8aeef9eb410844093c8b3b1ed8200
Your Name <you@example.com>
2025-09-11 10:04:19 +0300

    2.8 pre-solution

---
+++ Sept11/t2_8.py
@@ -0,0 +1,11 @@
+a, b = [int(d) for d in input().split()]
+print(a,b)
+
+if b>a:
+    print("b is larger than a")
+    print("b equals", b)
+else:
+    print("a is larger than b")
+    print("a equals", a)
+
+print("Ready!")
