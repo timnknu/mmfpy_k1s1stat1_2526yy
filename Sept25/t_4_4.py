6ce0dc2f85af43123680b994ad9b8ba879e080bf
Your Name <you@example.com>
2025-09-25 09:15:17 +0300

    version2, with continue

--- Sept25/t_4_4.py
+++ Sept25/t_4_4.py
@@ -4,8 +4,11 @@ while True:
     a = int(input())
     if a == 0:
         break
-    if a%2 == 1:
-        # число непарне
-        i += 1  # i = i + 1
-        sum_of_selected += a
+    # пропустити подальшу частину блока коду, якщо число парне (і, відповідно, виконати її, якщо воно непарне)
+    if a%2 == 0:
+        continue
+    # якщо програма вже добралася до цього рядочка, значить число було непарним
+    # число непарне
+    i += 1  # i = i + 1
+    sum_of_selected += a
 print(f'Number of non-zero values: {i}, and sum is {sum_of_selected}')
