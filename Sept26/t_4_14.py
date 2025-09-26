9b27fac63af716c7a3ca10df432e3f41944be65b
Your Name <you@example.com>
2025-09-26 08:56:50 +0300

    4.5

--- Sept26/t_4_14.py
+++ Sept26/t_4_14.py
@@ -1,6 +1,8 @@
-n = 178
+n = 17
 
-# 1 <= d <= n
-for d in range(1, n+1):
+# 1 < d <= n -- для задачі 4.5
+for d in range(2, n+1):
     if n%d == 0:
         print(f'число {n} ділиться на {d}')
+        break
+print('Готово!')
