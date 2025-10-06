2808560b7e6ef527d0af54c866fad72b2606e69b
Your Name <you@example.com>
2025-10-06 13:58:18 +0300

    v0a

--- Oct02/eo_1004.py
+++ Oct02/eo_1004.py
@@ -1,7 +1,17 @@
-n = 19
-base = 2
+n = 197564
+base = 10
 digits = []
-digits.insert(0, 128)
-digits.insert(0, -8)
-digits.insert(0, 15)
+while n > 0:
+    d = n % base
+    #print(d)
+    digits.insert(0, d)
+    n = n // base # або n //= base
+print(digits)
+
+L1 = [digits[-1]]
+print("Список із лише останнього елемента", L1)
+L2 = digits[:-1]
+print("Зріз до останнього елемента НЕВКЛЮЧНО", L2)
+digits = L1 + L2
+
 print(digits)
