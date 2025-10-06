4d4b5159ea5eaa10913e5a9c1a9387a9b531d5be
Your Name <you@example.com>
2025-10-06 13:40:06 +0300

    v4a

--- Oct02/t_5_2.py
+++ Oct02/t_5_2.py
@@ -1,10 +1,15 @@
 lst = [1, 12, -8, 127, 15, 24, -1]
 
-num_elements = len(lst)
-print("Загалом у нас є", num_elements)
-for i in range(num_elements):
-    print(f"же елемент, отриманий за індексом {i} рівний", lst[i])
-    if lst[i] >= 0:
-        lst[i] = lst[i] + 2
-print(lst)
+print(list(enumerate(lst)))
 
+for i, e in enumerate(lst):
+    print(f"Індекс {i}, а відповідний елемент списка: {e}")
+
+
+# num_elements = len(lst)
+# print("Загалом у нас є", num_elements)
+# for i in range(num_elements):
+#     print(f"же елемент, отриманий за індексом {i} рівний", lst[i])
+#     if lst[i] >= 0:
+#         lst[i] = lst[i] + 2
+# print(lst)
