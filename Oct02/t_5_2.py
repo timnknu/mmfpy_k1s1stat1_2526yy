8bee1c2150f2f530cfa63d1778d7f09482d5045c
Your Name <you@example.com>
2025-10-06 13:42:18 +0300

    v5

--- Oct02/t_5_2.py
+++ Oct02/t_5_2.py
@@ -3,13 +3,7 @@ lst = [1, 12, -8, 127, 15, 24, -1]
 print(list(enumerate(lst)))
 
 for i, e in enumerate(lst):
-    print(f"Індекс {i}, а відповідний елемент списка: {e}, і він рівний {lst[i]}")
-
-
-# num_elements = len(lst)
-# print("Загалом у нас є", num_elements)
-# for i in range(num_elements):
-#     print(f"же елемент, отриманий за індексом {i} рівний", lst[i])
-#     if lst[i] >= 0:
-#         lst[i] = lst[i] + 2
-# print(lst)
+    if e >= 0:
+        lst[i] = e + 2
+        # але так теж робити не дуже добре
+print(lst)
