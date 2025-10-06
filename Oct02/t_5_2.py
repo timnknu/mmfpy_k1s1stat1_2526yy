7d1008c25857c8349f02dc2fffbd396b4ce19db7
Your Name <you@example.com>
2025-10-06 13:27:51 +0300

    v1a

--- Oct02/t_5_2.py
+++ Oct02/t_5_2.py
@@ -1,9 +1,13 @@
 lst = [1, 12, -8, 127, 15, 24, -1]
 
+new_list = []
+
 for e in lst:
     if e >= 0:
         print("Цей елемент НЕвід'ємний:", e)
-        e = e + 2
+        new_list.append(e + 2)
     else:
         print("Цей елемент від'ємний:", e)
-print(lst)
+        new_list.append(e)
+print("Початковий список", lst)
+print("Новий список:", new_list)
