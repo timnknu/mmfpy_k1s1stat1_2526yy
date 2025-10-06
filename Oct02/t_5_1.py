1fdbc08ba2b22d106108840cea014d7bbc1aa010
Your Name <you@example.com>
2025-10-06 13:06:50 +0300

    v2

--- Oct02/t_5_1.py
+++ Oct02/t_5_1.py
@@ -1,4 +1,17 @@
-lst = [1, 15, -8, 88, 21, 53.7]
+n = int(input())
+lst_elements = input()
+print("кількість елементів:", n)
+print("другий рядок:", lst_elements)
+print("другий рядок та .split():", lst_elements.split())
+
+lst = []
+for e in lst_elements.split():
+    print("черговий елемент:", e)
+    lst.append(int(e))
+print("Сформований список lst:", lst)
+
+if n != len(lst):
+    print("Ой, а в списку то не та кількість елементів, яку обіцяли!")
 
 if len(lst)==0:
     print("елементів немає!")
