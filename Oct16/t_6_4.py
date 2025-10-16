4835bd6cc890a80413c15429b1b051fa2d813b91
Your Name <you@example.com>
2025-10-16 09:56:48 +0300

    v3

--- Oct16/t_6_4.py
+++ Oct16/t_6_4.py
@@ -1,4 +1,4 @@
-s = '''    Dear       friends,
+text = '''    Dear       friends,
 декілька        пропусків розміщені між двома словами (простіше
 кажучи, якщо     слова розділені більш         ніж одним пропуском, тоді усі
 пропуски крім одного - зайві)
@@ -7,24 +7,28 @@ Let me ...   '''
 s = "This  is a simple test"
 #s= ""
 
-W = [] # тут має бути список слів із рядка s,
-# де під "словом" мається на увазі частина рядка,
-# яка відділена від інших частин хоча б одним пробілом
+def my_split(s):
+    W = []  # тут має бути список слів із рядка s,
+    # де під "словом" мається на увазі частина рядка,
+    # яка відділена від інших частин хоча б одним пробілом
 
-# наш аналог функції .split()
-separators = [" ", "\n", ","]
-current_word = ""
-for ch in s:
-    if ch not in separators:
-        current_word = current_word + ch
-    else:
-        if current_word != "":
-            W.append(current_word)
-        current_word = ""
-if current_word != "":
-    W.append(current_word)
+    # наш аналог функції .split()
+    separators = [" ", "\n", ","]
+    current_word = ""
+    for ch in s:
+        if ch not in separators:
+            current_word = current_word + ch
+        else:
+            if current_word != "":
+                W.append(current_word)
+            current_word = ""
+    if current_word != "":
+        W.append(current_word)
 
-print("List of words:", W)
+    return W
+
+lstw = my_split(text)
+print("List of words:", lstw)
 
 # наш аналог функції .join()
 res = ""
@@ -32,8 +36,8 @@ res = ""
 #     res = res + elem + " "
 # if len(W)>0:
 #     res = res + W[-1]
-for i, elem in enumerate(W):
-    if i == len(W)-1:
+for i, elem in enumerate(lstw):
+    if i == len(lstw)-1:
         res = res + elem
     else:
         res = res + elem + " "
