8f8b60be431e79f6c6ed8e16031176053f7635e7
Your Name <you@example.com>
2025-10-24 09:58:35 +0300

    v2

--- Oct24/t_7_10.py
+++ Oct24/t_7_10.py
@@ -1,6 +1,6 @@
 # функція, яка перетворює число з десяткової системи у представлення в іншій системі числення
 def dec_to_other(n, base): # n - задане число у десятковій системі, base - основа нової системи числення
-    digits = []
+    res = ""
     while n > 0:
         d = n % base
         #print(d)
@@ -8,10 +8,10 @@ def dec_to_other(n, base): # n - задане число у десятковій
             d_str = str(d)
         else:
             d_str = chr(ord('A') + d - 10)
-        digits.insert(0, d_str)
+        res = d_str + res
+        #print(d_str, ">>", res)
         n = n // base # або n //= base
-    #return digits
-    return "".join(digits)
+    return res
 
 
 print(dec_to_other(255123, 16))
