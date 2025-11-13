8e776a02efffe9c0c234b345f8b658417c931af4
Your Name <you@example.com>
2025-11-13 09:52:27 +0200

    v0

--- Nov13/t_13_1.py
+++ Nov13/t_13_1.py
@@ -1,11 +1,11 @@
-infile = open('inp_13_1.txt', 'rb')
+infile = open('inp_13_1.txt', 'rt')
 
 # s = infile.readlines()
 # print(s)
 
 for line in infile:
-    #print(line)
-    for c in line:
-        print(c, bin(c))
+    #print(line[:-1])
+    if line.find(") ") == -1:
+        print(line.rstrip())
 
 infile.close()
