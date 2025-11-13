a74620d511b343fac13a1fe1628e161314e9e1d7
Your Name <you@example.com>
2025-11-13 10:07:03 +0200

    v1(d)

--- Nov13/t_13_1.py
+++ Nov13/t_13_1.py
@@ -1,15 +1,17 @@
 infile = open('inp_13_1.txt', 'rt')
 
-max_line_length = -10
-the_longest_line = ''
+
+the_longest_line = infile.readline()
+max_line_length = len(the_longest_line)
 
 for line in infile:
     line = line.rstrip()
-    #print(len(line), line)
+    print("I'm reading line:", len(line), line)
     if len(line) > max_line_length:
         max_line_length = len(line)
         the_longest_line = line
 
 infile.close()
 
+print('-'*80)
 print(the_longest_line)
