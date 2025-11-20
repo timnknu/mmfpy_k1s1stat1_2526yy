17b315ec2d0b88a0393456ab58043286f5ec1667
Your Name <you@example.com>
2025-11-20 17:02:00 +0200

    9537-v4

--- Nov20/eo_9537.py
+++ Nov20/eo_9537.py
@@ -9,7 +9,33 @@ def read_matrix_from_file(file_name):
 #
 
 A = read_matrix_from_file('inp1.txt')
-print(A)
+print('Here is our original matrix')
+for row in A:
+    print('here is the row of A:', row)
+print(A[2][1])
+# r = A[0.....len(A)-1] == A[0...A_n_rows-1]
+# r[0...len(r)-1s] == r[0...A_n_cols-1]
+# =>
+# A[0...A_n_rows-1][0...A_n_cols-1] <-> A[i][j]: 0<=i<=A_n_rows-1, 0<=j<=A_n_cols-1
+
+# B[i][j] = A[j][i]
+A_n_rows = len(A)
+A_n_cols = len(A[0])
+print(f"матриця має {A_n_rows} рядк(и/ів) та {A_n_cols} стопвці(в)")
+
+B_n_rows = A_n_cols
+B_n_cols = A_n_rows
+# B[0...B_n_rows-1][0..B_n_cols-1]
+# тепер в B[i][j] елемент матриці B треба записати A[j][i] елемент матриці A
+
+B = []
+for i in range(B_n_rows):
+    row = []
+    for j in range(B_n_cols):
+        row.append(0)
+    B.append(row)
+
+print(B)
 
 
 # обробка (транспонування матриці)
