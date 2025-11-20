3d95c87ac79855c5f2f63ea69a6ff0ce78eb90c4
Your Name <you@example.com>
2025-11-20 16:33:40 +0200

    9537-v3

--- Nov20/eo_9537.py
+++ Nov20/eo_9537.py
@@ -1,12 +1,16 @@
-
-with open('inp1.txt') as f:
-    M = []
-    for line in f:
-        line_cleaned = line.rstrip('\r\n')
-        vals = [float(elem) for elem in line_cleaned.split()]
-        M.append(vals)
-    print(M)
+def read_matrix_from_file(file_name):
+    with open(file_name) as f:
+        M = []
+        for line in f:
+            line_cleaned = line.rstrip('\r\n')
+            vals = [float(elem) for elem in line_cleaned.split()]
+            M.append(vals)
+    return M
 #
+
+A = read_matrix_from_file('inp1.txt')
+print(A)
+
 
 # обробка (транспонування матриці)
 
