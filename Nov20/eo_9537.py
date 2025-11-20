458d9bb3aa9234a8263b6e74effca7673c86e85e
Your Name <you@example.com>
2025-11-20 16:32:06 +0200

    9537-v2

--- Nov20/eo_9537.py
+++ Nov20/eo_9537.py
@@ -1,13 +1,12 @@
-f = open('inp1.txt')
 
-M = []
-for line in f:
-    line_cleaned = line.rstrip('\r\n')
-    vals = [float(elem) for elem in line_cleaned.split()]
-    M.append(vals)
-print(M)
-
-f.close()
+with open('inp1.txt') as f:
+    M = []
+    for line in f:
+        line_cleaned = line.rstrip('\r\n')
+        vals = [float(elem) for elem in line_cleaned.split()]
+        M.append(vals)
+    print(M)
+#
 
 # обробка (транспонування матриці)
 
