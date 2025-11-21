8b010dcd6ae81844a45279c1f824977239c8d463
Your Name <you@example.com>
2025-11-21 09:33:08 +0200

    exceptions-v2

--- Nov21/ex1.py
+++ Nov21/ex1.py
@@ -4,16 +4,21 @@ is_debigging = True
 #is_debigging = False
 
 def read_matrix(file_name):
+    all_uniq_lengths = set()
     with open(file_name) as f:
         M = []
         for line in f:
             line_cleaned = line.rstrip('\r\n')
             vals = [float(elem) for elem in line_cleaned.split()]
             M.append(vals)
+            n_elems = len(vals)
+            all_uniq_lengths.add(n_elems)
+            #all_uniq_lengths = all_uniq_lengths | {n_elems}
+    print(all_uniq_lengths)
     return M
 #
 
-file_name = 'file_name'
+file_name = 'inp1.txt'
 
 try:
     A = read_matrix(file_name)
