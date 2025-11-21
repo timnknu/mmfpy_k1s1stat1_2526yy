478f4777b8a03e9d425e2f70e902433a066ff228
Your Name <you@example.com>
2025-11-21 09:25:38 +0200

    exceptions-v1

--- Nov21/ex1.py
+++ Nov21/ex1.py
@@ -3,16 +3,21 @@ import traceback
 is_debigging = True
 #is_debigging = False
 
-file_name = 'file_name'
-
-try:
+def read_matrix(file_name):
     with open(file_name) as f:
         M = []
         for line in f:
             line_cleaned = line.rstrip('\r\n')
             vals = [float(elem) for elem in line_cleaned.split()]
             M.append(vals)
-    print(M)
+    return M
+#
+
+file_name = 'file_name'
+
+try:
+    A = read_matrix(file_name)
+    print(A)
 except FileNotFoundError:
     print("файл відсутній")
     if is_debigging:
