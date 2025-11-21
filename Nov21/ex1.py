28345a174b3a20039d5a073646e856a90b45a1ff
Your Name <you@example.com>
2025-11-21 09:39:35 +0200

    exceptions-v3

--- Nov21/ex1.py
+++ Nov21/ex1.py
@@ -14,7 +14,11 @@ def read_matrix(file_name):
             n_elems = len(vals)
             all_uniq_lengths.add(n_elems)
             #all_uniq_lengths = all_uniq_lengths | {n_elems}
-    print(all_uniq_lengths)
+    if len(all_uniq_lengths)==1:
+        print("з матрицею все гаразд")
+    else:
+        print("різні рядки матриці мають різну кількість елементів")
+        raise ValueError("некоректна структура матриці")
     return M
 #
 
@@ -23,6 +27,29 @@ file_name = 'inp1.txt'
 try:
     A = read_matrix(file_name)
     print(A)
+
+    A_n_rows = len(A)
+    A_n_cols = len(A[0])
+    print(f"матриця має {A_n_rows} рядк(и/ів) та {A_n_cols} стопвці(в)")
+
+    B_n_rows = A_n_cols
+    B_n_cols = A_n_rows
+    # B[0...B_n_rows-1][0..B_n_cols-1]
+    # тепер в B[i][j] елемент матриці B треба записати A[j][i] елемент матриці A
+
+    # обробка (транспонування матриці)
+    B = []
+    for i in range(B_n_rows):
+        row = []
+        for j in range(B_n_cols):
+            row.append(A[j][i])
+        B.append(row)
+
+    print('Here is our transposed matrix')
+    for row in B:
+        print('here is the row of B:', row)
+
+
 except FileNotFoundError:
     print("файл відсутній")
     if is_debigging:
