03dd8c8ddc837b903573a365fbb49b0688b86b1f
Your Name <you@example.com>
2025-11-27 10:38:38 +0200

    10.4+manual solution check

--- Nov27/t_10_4.py
+++ Nov27/t_10_4.py
@@ -8,4 +8,21 @@ A = [row1, row2, row3]
 #A[0] = row2
 print(det(A))
 
-print(transpose(A))
+b = [-1, -2, 15]
+
+x = []
+for i in range(len(b)):
+    A_transposed = transpose(A)
+    A_transposed[i] = b
+    #A_with_b_at_i = transpose(A_transposed)
+    #sol_i = det(A_with_b_at_i) / det(A)
+    sol_i = det(A_transposed) / det(A)
+    print(f'{i}-th variable:', sol_i)
+    x.append(sol_i)
+print(x)
+
+row = A[2]
+res = 0
+for i in range(len(b)):
+    res += row[i] * x[i]
+print(res)
