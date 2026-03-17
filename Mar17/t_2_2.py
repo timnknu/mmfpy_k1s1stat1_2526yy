7b68e31bc9e5a04c3b2a8ade506596e21264a11e
Your Name <you@example.com>
2026-03-17 12:09:52 +0200

    v4

--- Mar17/t_2_2.py
+++ Mar17/t_2_2.py
@@ -1,4 +1,6 @@
 import turtle
+import random
+import math
 
 turtle.speed(1)
 
@@ -8,22 +10,33 @@ class Triangle:
         self._vertex1 = (x1, y1)
         self._vertex2 = (x2, y2)
         self._color = "black"
+        self._rot_angle = 0
+
+    def set_rotation(self, alpha):
+        self._rot_angle = alpha
 
     def set_position(self, x, y):
         self._position = (x, y)
+
     def set_color(self, color):
         self._color = color
 
+    def _rotate_vec(self, x_old, y_old):
+        x_rot = math.cos(self._rot_angle) * x_old + math.sin(self._rot_angle) * y_old
+        y_rot = -math.sin(self._rot_angle) * x_old + math.cos(self._rot_angle) * y_old
+        return x_rot, y_rot
+
+
     def draw(self):
         x0, y0 = self._position
-        x1, y1 = self._vertex1
-        x2, y2 = self._vertex2
+        x1, y1 = self._rotate_vec(self._vertex1[0], self._vertex1[1])
+        x2, y2 = self._rotate_vec(*self._vertex2)
 
         turtle.penup()
         turtle.goto(x0, y0)
         turtle.pendown()
         turtle.color(self._color)
-        turtle.fillcolor('yellow')
+        turtle.fillcolor(self._color)
 
         turtle.begin_fill()
         turtle.goto(x0+x1, y0+y1)
@@ -31,13 +44,24 @@ class Triangle:
         turtle.goto(x0, y0)
         turtle.end_fill()
 
-tr = Triangle(120, 50, 30, 180)
-tr.set_position(50, 60)
+tr = Triangle(100, 0, 0, 50)
 tr.set_color('lime green')
-tr.draw()
 
-another_tr = Triangle(-120, -150, -30, -80)
-another_tr.draw()
+for itr in range(10):
+    tr.set_color('white')
+    tr.draw()
+    tr.set_color('lime green')
+    tr.set_rotation(2*math.pi/10 * itr)
+    tr.draw()
+
+
+# for i in range(10):
+#     R = [random.randint(-100, 100) for c in range(6)]
+#
+#     tr = Triangle(R[0], R[1], R[2], R[3])
+#     tr.set_position(R[4], R[5])
+#     tr.set_color('lime green')
+#     tr.draw()
 
 
 turtle.mainloop()
