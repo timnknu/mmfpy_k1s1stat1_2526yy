c119432bcf66d426aa23c949080973f8ca0e7d45
Your Name <you@example.com>
2026-03-17 11:16:09 +0200

    v1

--- Mar17/t_2_2.py
+++ Mar17/t_2_2.py
@@ -1,7 +1,19 @@
 import turtle
 
 turtle.speed(1)
-turtle.forward(150)
+
+x0, y0 = -100, -80
+x1, y1 = 120, 150
+x2, y2 = -120, 190
+
+
+turtle.penup()
+turtle.goto(x0, y0)
+turtle.pendown()
+
+turtle.goto(x0+x1, y0+y1)
+turtle.goto(x0+x2, y0+y2)
+turtle.goto(x0, y0)
 
 turtle.mainloop()
 #turtle.exitonclick()
