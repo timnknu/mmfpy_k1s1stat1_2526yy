a5035923ca03cc2143843978090524303c8cdc80
Your Name <you@example.com>
2026-03-17 11:47:39 +0200

    v2

--- Mar17/t_2_2.py
+++ Mar17/t_2_2.py
@@ -2,18 +2,38 @@ import turtle
 
 turtle.speed(1)
 
-x0, y0 = -100, -80
-x1, y1 = 120, 150
-x2, y2 = -120, 190
+class Triangle:
+    def __init__(self, x1, y1, x2, y2):
+        self._position = (0, 0)
+        self._vertex1 = (x1, y1)
+        self._vertex2 = (x2, y2)
+        #self._color = "black"
 
+    def set_position(self, x, y):
+        self._position = (x, y)
+    def set_color(self, color):
+        self._color = color
 
-turtle.penup()
-turtle.goto(x0, y0)
-turtle.pendown()
+    def draw(self):
+        x0, y0 = self._position
+        x1, y1 = self._vertex1
+        x2, y2 = self._vertex2
 
-turtle.goto(x0+x1, y0+y1)
-turtle.goto(x0+x2, y0+y2)
-turtle.goto(x0, y0)
+        turtle.penup()
+        turtle.goto(x0, y0)
+        turtle.pendown()
+
+        turtle.goto(x0+x1, y0+y1)
+        turtle.goto(x0+x2, y0+y2)
+        turtle.goto(x0, y0)
+
+tr = Triangle(120, 50, 30, 180)
+tr.set_position(50, 60)
+tr.draw()
+
+another_tr = Triangle(-120, -150, -30, -80)
+another_tr.draw()
+
 
 turtle.mainloop()
 #turtle.exitonclick()
