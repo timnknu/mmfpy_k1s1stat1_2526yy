d4776a9e4a589a7f098818e26eb8d638cd114b86
Your Name <you@example.com>
2026-03-31 11:54:09 +0300

    v3

--- Mar31/t_421.py
+++ Mar31/t_421.py
@@ -33,9 +33,10 @@ class Circle(Figure):
         turtle.pendown()
         turtle.circle(self._r)
 
-class Square(Figure):
-    def __init__(self, x0, y0, side):
-        self._a = side
+class Rectangle(Figure):
+    def __init__(self, x0, y0, sideX, sideY):
+        self._a = sideX
+        self._b = sideY
         super().__init__(x0, y0)
     def set_side(self, a):
         self._a = a
@@ -43,8 +44,8 @@ class Square(Figure):
         turtle.penup()
         turtle.goto(self._x0, self._y0)
         turtle.pendown()
-        turtle.goto(self._x0, self._y0 + self._a)
-        turtle.goto(self._x0 + self._a, self._y0 + self._a)
+        turtle.goto(self._x0, self._y0 + self._b)
+        turtle.goto(self._x0 + self._a, self._y0 + self._b)
         turtle.goto(self._x0 + self._a, self._y0)
         turtle.goto(self._x0, self._y0)
 
@@ -53,7 +54,7 @@ class Square(Figure):
 #
 turtle.speed(1)
 
-s = Square(100, 100, 80)
+s = Rectangle(100, 100, 80, 120)
 s.set_color('red')
 s.show()
 s.show()
