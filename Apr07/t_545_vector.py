0e37ef99a2b4811f86b867465e3267754d08fe62
Your Name <you@example.com>
2026-04-07 11:24:50 +0300

    v1

--- Apr07/t_545_vector.py
+++ Apr07/t_545_vector.py
@@ -1,14 +1,31 @@
+import copy
+
 class Vector:
     def __init__(self, lst):
-        self._elems = lst
+        self._elems = copy.copy(lst)
     def __str__(self):
         #estr = [str(e) for e in self._elems]
         estr = map(str, self._elems)
         s = ', '.join(estr)
         return f"Vec ({s})"
+    def __add__(self, other):
+        # assert len(self._elems) == len(other._elems):
+        if len(self._elems) != len(other._elems):
+            raise ValueError("Вектори мають мати однакову кількість елементів")
+        #
+        new_elems = []
+        for i in range(len(self._elems)):
+            c = self._elems[i] + other._elems[i]
+            new_elems.append(c)
+        new_vec = Vector(new_elems)
+        return new_vec
 
-obj = Vector([1,2,3])
-print(obj)
 
-# s = 'xyz'.join(['ABC', 'FGI', 'KKKKKK'])
-# print(s)
+a = Vector([1,2,3])
+b = Vector([100, 500, 800])
+print(a)
+print(b)
+print(a + b)
+
+
+
