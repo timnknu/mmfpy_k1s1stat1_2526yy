7b20b25d2f1badf9245f19d4939721b79d328333
Your Name <you@example.com>
2026-04-14 10:51:50 +0300

    v2

---
+++ Apr07/ex1.py
@@ -0,0 +1,11 @@
+a = 12
+b = 35.5
+c = []
+
+
+obj = a
+
+print(isinstance(obj, int))
+print(isinstance(obj, float))
+print(isinstance(obj, (int, float)))
+print(isinstance(obj, list))
--- Apr07/t_545_vector.py
+++ Apr07/t_545_vector.py
@@ -9,18 +9,32 @@ class Vector:
         s = ', '.join(estr)
         return f"Vec ({s})"
     def __add__(self, other):
-        # assert len(self._elems) == len(other._elems):
-        if len(self._elems) != len(other._elems):
-            raise ValueError("Вектори мають мати однакову кількість елементів")
-        #
-        new_elems = []
-        for i in range(len(self._elems)):
-            c = self._elems[i] + other._elems[i]
-            new_elems.append(c)
-        # for a,b in zip(self._elems, other._elems):
-        #     new_elems.append(a+b)
-        new_vec = Vector(new_elems)
-        return new_vec
+        if isinstance(other, (float, int)):
+            # other -- число
+            new_elems = []
+            for i in range(len(self._elems)):
+                c = self[i] + other
+                new_elems.append(c)
+            new_vec = Vector(new_elems)
+            return new_vec
+        elif isinstance(other, Vector):
+            # assert len(self._elems) == len(other._elems):
+            if len(self._elems) != len(other._elems):
+                raise ValueError("Вектори мають мати однакову кількість елементів")
+            #
+            new_elems = []
+            for i in range(len(self._elems)):
+                c = self[i] + other[i]
+                new_elems.append(c)
+            # for a,b in zip(self._elems, other._elems):
+            #     new_elems.append(a+b)
+            new_vec = Vector(new_elems)
+            return new_vec
+        else:
+            raise ValueError("Невідомий тип доданка")
+    def __radd__(self, other):
+        #return self.__add__(other)
+        return self + other
     def __getitem__(self, item):
         return self._elems[item]
     def __setitem__(self, key, value):
@@ -29,15 +43,8 @@ class Vector:
 
 
 a = Vector([1,2,3])
-
-print(a[0])
-a[0] = 125
-print(a)
-#
-# b = Vector([100, 500, 800])
-# print(a)
-# print(b)
-# print(a + b)
-
-
-
+b = Vector([1000,500,800])
+print(a + b)
+print(a + 12)
+print(12 + a)
+#print(a + 'рядок')
