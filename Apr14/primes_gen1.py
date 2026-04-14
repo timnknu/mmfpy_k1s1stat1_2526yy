cd31662328647fa38e23ee9616c398c72c9b3af7
Your Name <you@example.com>
2026-04-14 11:39:31 +0300

    v2a

--- Apr14/primes_gen1.py
+++ Apr14/primes_gen1.py
@@ -1,14 +1,20 @@
+def get_primes(max_n):
+    known_primes = []
 
-known_primes = []
+    for candidate in range(2, max_n):
+        is_prime = True
+        for d in known_primes :
+            if candidate % d == 0:
+                is_prime = False
+                break
+            if d > candidate**0.5:
+                break
+        if is_prime:
+            known_primes.append(candidate)
+    return known_primes
 
-for candidate in range(2, 100):
-    is_prime = True
-    for d in known_primes :
-        if candidate % d == 0:
-            is_prime = False
-            break
-        if d > candidate**0.5:
-            break
-    if is_prime:
-        print(candidate)
-        known_primes.append(candidate)
+L1 = get_primes(100)
+L2 = get_primes(100)
+for i in range(len(L1)-1):
+    if L1[i+1] - L2[i] == 2:
+        print(L1[i+1], L2[i])
