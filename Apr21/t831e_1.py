619576db458a494262c8d7e2b1c4d6a914bbf59e
Your Name <you@example.com>
2026-04-21 11:59:10 +0300

    v3

--- Apr21/t831e_1.py
+++ Apr21/t831e_1.py
@@ -1,17 +1,22 @@
 import math
 
-N = 1000
-eps = 1e-5 # 10**(-5) = 0.00001
-x = 10
-an = x
-y = an
-n = 1
-while True:
-    an = an * (-x**2/(2*n+1)/2/n)
-    y = y + an
-    print(n, ':', an, y)
-    if abs(an) < eps:
+def sg(x):
+    an = x
+    yield x
+    n = 1
+    while True:
+        an = an * (-x**2/(2*n+1)/2/n)
+        yield an
+        n += 1
+
+
+eps = 1e-5  # 10**(-5) = 0.00001
+y = 0
+x = 12.0
+for term in sg(x):
+    y += term
+    print( ':', term, y)
+    if abs(term) < eps:
         break
-    n += 1
 print(y)
 print(math.sin(x))
