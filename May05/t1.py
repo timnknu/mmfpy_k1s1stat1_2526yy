23c70b40bfb2ea776f4f06d3050dc0a90532083b
Your Name <you@example.com>
2026-05-05 11:46:03 +0300

    v0b

--- May05/t1.py
+++ May05/t1.py
@@ -2,11 +2,30 @@ import tkinter
 
 root = tkinter.Tk()
 
+rates = {
+    'USD': {'UAH': 36.57, 'EUR': 0.92,  'USD': 1},
+    'EUR': {'UAH': 39.78, 'EUR': 1,     'USD': 1.09},
+    'UAH': {'UAH': 1,     'EUR': 0.025, 'USD': 0.027}
+}
+
+# cFrom = 'UAH'
+# cTo = 'USD'
+# k = rates[cFrom][cTo]
+# print(k)
+
 def btn_onlick():
     print("Button was clicked!", usrval.get())
-    usrval.set('HELLO')
 
-usrval = tkinter.StringVar(value="початкове значення")
+    s = usrval.get()
+    d = s.split()
+    v = float(d[0])
+    cFrom = d[1]
+    k = rates[cFrom]['UAH']
+    res = v * k
+
+    usrval.set(str(res))
+
+usrval = tkinter.StringVar(value="81.8 USD")
 inpvalfield = tkinter.Entry(root)#, textvariable=usrval)
 inpvalfield['textvariable'] = usrval
 inpvalfield.pack()
